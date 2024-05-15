e929b815ef58ff694f594a12240826572c849090
mr_inceptionist <self.dipak@gmail.com>
2024-05-15 23:38:27 +0530

    created tasks fragment and attached it with viewpager.

--- app/src/main/java/com/example/getitdone/MainActivity.kt
+++ app/src/main/java/com/example/getitdone/MainActivity.kt
@@ -5,11 +5,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.getitdone.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.pager.adapter=PagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout,binding.pager){ tab,position->
+
+//            when(position){
+//                0->{
+//                    tab.text="Tasks"
+//
+//                }
+//            }
+            
+            // OR 
+            
+            tab.text="Tasks"
+
+
+        }.attach()
+    }
+
+    private inner class PagerAdapter(activity:FragmentActivity):FragmentStateAdapter(activity){
+        override fun getItemCount(): Int=1
+
+
+        override fun createFragment(position: Int): Fragment {
+
+            return TasksFragment()
+        }
 
     }
 }
