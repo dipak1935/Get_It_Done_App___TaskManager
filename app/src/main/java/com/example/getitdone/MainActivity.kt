179e234aec17e3d5a95c0602fd9562872f082715
mr_inceptionist <self.dipak@gmail.com>
2024-05-16 00:35:54 +0530

    Added a floating action button to the project.

--- app/src/main/java/com/example/getitdone/MainActivity.kt
+++ app/src/main/java/com/example/getitdone/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.getitdone
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,11 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.getitdone.databinding.ActivityMainBinding
+import com.example.getitdone.databinding.DialogAddTaskBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -37,6 +41,20 @@ class MainActivity : AppCompatActivity() {
 
 
         }.attach()
+
+        binding.fab.setOnClickListener {
+
+            val dialogBinding=DialogAddTaskBinding.inflate(layoutInflater)
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Add new task")
+                .setView(dialogBinding.root)
+                .setPositiveButton("SAVE"){_,_->
+                    Toast.makeText(this,"Item have been saved",Toast.LENGTH_SHORT).show()
+
+                }
+                .setNegativeButton("CANCEL"){_,_->}
+                .show()
+        }
     }
 
     private inner class PagerAdapter(activity:FragmentActivity):FragmentStateAdapter(activity){
